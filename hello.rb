3f83e0b22c1990ed90d019f48d21545538de20b2
Luis Fernando Mejia <lf.anaya21@info.uas.edu.mx>
2025-10-24 07:22:27 -0700

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,4 +1,6 @@
-# This is an unwanted but committed change
+# Default is World
+# Author: Luis Fernando Anaya Mejia (lf.anaya21@info.uas.edu.mx)
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
+
